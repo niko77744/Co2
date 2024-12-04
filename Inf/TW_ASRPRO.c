52016f84d0f998179dabb8caa71ff1e085136edd
niko77744 <1836045035@qq.com>
2024-12-04 08:45:47 +0800

    feat(Inf): 添加 TW_ASRPRO 初始化和数据发送功能 - 在 .gitignore 中添加 MDK-ARM 目录忽略项

--- Inf/TW_ASRPRO.c
+++ Inf/TW_ASRPRO.c
@@ -1,1 +1,10 @@
 #include "TW_ASRPRO.h"
+
+
+void TW_ASRPRO_Init(void) {
+    MX_USART2_UART_Init();
+}
+
+void TW_ASRPRO_SendData(uint8_t* data, uint8_t size) {
+    HAL_UART_Transmit(&huart2, data, size, HAL_MAX_DELAY);
+}
--- Inf/TW_ASRPRO.h
+++ Inf/TW_ASRPRO.h
@@ -1,6 +1,11 @@
 #ifndef __TW_ASRPRO_H__
 #define __TW_ASRPRO_H__
 
+#include "usart.h"
+#include "main.h"
+
+void TW_ASRPRO_Init(void);
+void TW_ASRPRO_SendData(uint8_t* data, uint8_t size);
 
 
 #endif /* __TW_ASRPRO_H__ */
